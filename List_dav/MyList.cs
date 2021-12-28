1c53cfff8bd74e48d45509a076f81f6038f2735b
onise chaganava <onise.onise25@gmail.com>
2021-12-28 16:17:05 +0400

    Add project files.

---
+++ List_dav/MyList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_dav
+{
+    class MyList<T>
+    {
+        static int Capacity = 1000;
+        T[] myarr = new T[Capacity];
+        private int MySize;
+
+        public MyList()
+        {
+            MySize = 0;
+        }
+
+        public bool isEmpty() { return MySize==0; }
+        public void Delete(int position) {
+            if (isEmpty())
+            {
+                Console.WriteLine("!!!List is empty !!!");
+            }else if (position < 0 || position >= MySize)
+            {
+                Console.WriteLine("!!! Wrong position !!!");
+            }
+            else
+            {
+                for(int i = position; i < MySize; i++)
+                {
+                    myarr[i] = myarr[i + 1];
+                }
+
+                MySize--;
+            }
+        }
+        public void Add(T element, int position) {
+            if (MySize == Capacity ||(position < 0 || position > MySize))
+            {
+                Console.WriteLine("!!! No space for list element or wrong position !!!");
+            }else
+            {
+                for(int i = MySize; i > position; i--)
+                {
+                    myarr[i] = myarr[i - 1];
+                }
+                myarr[position] = element;
+                MySize++;
+            }
+        }
+        public int Count() { return MySize; }
+        public T ElementByIndex(int index) {
+            if (index < 0 || index > MySize - 1)
+            {
+                Console.WriteLine("!!! wrong index !!!");
+                return myarr[-1];
+            }
+            else
+            {
+                return myarr[index];
+            }
+        }
+        public void show() {
+            for (int i= 0;i < MySize;i++)
+            {
+                if (i % 5 == 0) Console.WriteLine();
+                Console.Write($"{myarr[i]}    ");      
+            }
+        }
+
+    }
+}
---
+++ List_dav/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace List_dav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyList<int> i = new MyList<int>();
+
+            i.show();
+            i.Add(1, 0);
+            i.Add(2, 1);
+            i.Add(3, 2);
+            i.show();
+
+            i.Delete(0);
+            i.show();
+
+            Console.WriteLine($"number of elements: {i.Count()}");
+
+            Console.WriteLine($"Element by index:  index = 1 & element= {i.ElementByIndex(1)}");
+            //Console.WriteLine($"Element by index:  index = 5 & element= {i.ElementByIndex(5)}");
+        }
+    }
+}
